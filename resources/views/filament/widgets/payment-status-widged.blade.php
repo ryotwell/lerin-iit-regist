c4dd4a5bea354a39c501ac5fefca14f169367bb1
Ryo Otwell <ytryo789@gmail.com>
2024-11-07 02:28:58 +0800

    + cantik

--- resources/views/filament/widgets/payment-status-widged.blade.php
+++ resources/views/filament/widgets/payment-status-widged.blade.php
@@ -5,7 +5,7 @@ $approved = auth()->user()->payment->status === 'approved';
 @endphp
 <x-filament-widgets::widget>
     <x-filament::section>
-        <div class="flex justify-between mb-6">
+        <div class="flex justify-between mb-3">
             <div class="font-bold">
                 Status Pendaftaran
             </div>
@@ -21,41 +21,9 @@ $approved = auth()->user()->payment->status === 'approved';
                 @endif
             </div>
         </div>
-        <div class="mb-6">
-            
-        </div>
-        <div>
-            <div class="text-gray-600 mb-8">
-                <p class="mb-4">Langkah selanjutnya menyelesaikan biaya pendaftaran melalui transfer bank sebesar <span class="font-bold">Rp. 50.000</span> ke rekening berikut :</p>
-                <table>
-                    <tr>
-                        <td>Bank</td>
-                        <td>:</td>
-                        <td class="font-medium">BRI</td>
-                    </tr>
-                    <tr>
-                        <td>No. Rekening</td>
-                        <td>:</td>
-                        <td class="font-medium">92817212129</td>
-                    </tr>
-                    <tr>
-                        <td>Atas Nama</td>
-                        <td>:</td>
-                        <td class="font-medium">Muhammad Farid Hidayat</td>
-                    </tr>
-                </table>
-            </div>
-            <div class="text-gray-600 mb-8">
-                <p class="mb-4">Setelah melakukan pembayaran, silahkan upload bukti pembayaran melalui tombol di bawah ini.</p>
-
-                <a href="/panel/payments/{{ auth()->user()->payment->id }}/upload" class="bg-slate-900 dark:bg-slate-300 dark:text-slate-950 text-white px-4 py-2 rounded-xl hover:shadow-xl duration-100">
-                    Upload bukti pembayaran
-                </a>
-            </div>
-            <p class="text-gray-600 mb-8">
-                Jika anda sudah melakukan pembayaran, silahkan tunggu admin untuk melakukan konfirmasi pada pembayaran anda.
-            </p>
-            <x-admin-contact />
-        </div>
+        <a href="/panel/payments/{{ auth()->user()->payment->id }}/upload" class="text-sm underline text-blue-500 flex items-center">
+            Upload bukti pembayaran
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="ml-2 lucide lucide-move-right"><path d="M18 8L22 12L18 16"/><path d="M2 12H22"/></svg>
+        </a>
     </x-filament::section>
 </x-filament-widgets::widget>
