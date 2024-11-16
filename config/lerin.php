8c0678360539893c5218cfc4677be7596a1dc572
Zulzario Zaeri <ryotwell@icloud.com>
2024-11-16 14:20:22 +0800

    + update faq

--- config/lerin.php
+++ config/lerin.php
@@ -10,14 +10,4 @@ return [
         'phone' => '6285737074723',
         'whatsapp' => 'https://wa.me/6285737074723',
     ],
-    'faq' => [
-        [
-            'title' => 'Berapa jumlah orang dalam satu tim?',
-            'answer' => 'Anggota 2 orang, dan 1 orang sebagai penanggung jawab tim, jadi total 3 orang.',
-        ],
-        // [
-        //     'title' => 'Frequently Asked Questions',
-        //     'answer' => 'Frequently Asked Questions',
-        // ],
-    ],
 ];
--- resources/js/components/faq.tsx
+++ resources/js/components/faq.tsx
@@ -11,13 +11,20 @@ type FaqQuestionType = {
     answer: string
 }
 
+const faqQuestions: FaqQuestionType[] = [
+    {
+        title: 'Berapa jumlah orang dalam satu tim?',
+        answer: 'Anggota 2 orang, dan 1 orang sebagai penanggung jawab tim, jadi total 3 orang.'
+    },
+]
+
 export function Faq() {
     const [questions, setQuestions] = React.useState<FaqQuestionType[]>(faqQuestions)
 
     return (
         <Accordion type="single" collapsible className="w-full mt-6">
             {questions.map((question, index) => (
-                <AccordionItem key={index} value={`item-${index + 1}`}>
+                <AccordionItem key={index} value={`item-${index}`}>
                     <AccordionTrigger>{question.title}</AccordionTrigger>
                     <AccordionContent>{question.answer}</AccordionContent>
                 </AccordionItem>
