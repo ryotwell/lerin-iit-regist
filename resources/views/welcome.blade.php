80fc60f5b88f01b72572005402629835612fa269
Zulzario Zaeri <ryotwell@icloud.com>
2024-11-17 04:02:46 +0800

    ~ sesuaikan welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,14 +7,14 @@
     <link rel="canonical" href="https://iit.lerinntb.com" />
 	<meta property="og:locale" content="id_ID" />
 	<meta property="og:type" content="website" />
-	<meta property="og:title" content="{{ config('app.name', 'Laravel') }} - LERIN NTB (Lembaga Edukasi Riset & Inovasi Nusa Tenggara Barat)" />
+	<meta property="og:title" content="{{ config('app.name', 'Laravel') }} - {{ 'LERIN NTB (Lembaga Edukasi Riset & Inovasi Nusa Tenggara Barat)' }}" />
 	<meta property="og:description" content="UNHAZ x LERIN presents the Robotic Competition. Show your team's skill and innovation!" />
 	<meta property="og:url" content="https://iit.lerinntb.com" />
-	<meta property="og:site_name" content="LERIN NTB (Lembaga Edukasi Riset & Inovasi Nusa Tenggara Barat)" />
+	<meta property="og:site_name" content="{{ 'LERIN NTB (Lembaga Edukasi Riset & Inovasi Nusa Tenggara Barat)' }}" />
 	<meta property="og:image" content="{{ url('/thumbnail.jpg') }}" />
 	<meta name="twitter:card" content="summary_large_image" />
 
-    <title>{{ config('app.name', 'Laravel') }} - LERIN NTB (Lembaga Edukasi Riset & Inovasi Nusa Tenggara Barat)</title>
+    <title>{{ config('app.name', 'Laravel') }} - {{ 'LERIN NTB (Lembaga Edukasi Riset & Inovasi Nusa Tenggara Barat)' }}</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
