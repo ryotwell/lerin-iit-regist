c90b91b118e6b92dd7645c52c9e8e21bff78147d
Zulzario Zaeri <ryotwell@icloud.com>
2024-12-07 13:48:06 +0800

    + with payment

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,6 +12,8 @@ class User extends Authenticatable
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
+    protected $with = ['payment'];
+
     /**
      * The attributes that are mass assignable.
      *
